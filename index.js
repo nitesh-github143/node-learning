8fd8e3fd1d10526aef2b3a4845f27ff4137f6219
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-04-07 22:09:35 +0530

    switch statement

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const http = require('http')
+const fs = require('fs')
 
-const data = { age: 5 }
+const index = fs.readFileSync('index.html', 'utf-8')
+const data = fs.readFileSync('data.json', 'utf-8')
+
 const server = http.createServer((req, res) => {
     console.log(req.url)
+
+    switch (req.url) {
+        case '/':
+            res.setHeader('Content-Type', 'text/html')
+            res.end(index)
+            break
+        case '/api':
+            res.setHeader('Content-Type', 'application/json')
+            res.end(data)
+            break
+        default:
+            res.writeHead(404, 'Not Found')
+            res.end()
+    }
     console.log('server started')
-    res.setHeader('Dummy', 'Dummy header')
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify(data))
+    // res.setHeader('Dummy', 'Dummy header')
+
 })
 
-server.listen(8000)
+server.listen(8080)
