de881a1b62bb6093e8472046fa4780b61c4eacd9
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-04-08 04:55:04 +0530

    express

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
-const http = require('http')
-const fs = require('fs')
+const fs = require('fs');
+const express = require('express')
 
-const index = fs.readFileSync('index.html', 'utf-8')
-const data = fs.readFileSync('data.json', 'utf-8')
+const index = fs.readFileSync('index.html', 'utf-8');
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const products = data.products;
 
-const server = http.createServer((req, res) => {
-    console.log(req.url)
+const server = express()
 
-    switch (req.url) {
-        case '/':
-            res.setHeader('Content-Type', 'text/html')
-            res.end(index)
-            break
-        case '/api':
-            res.setHeader('Content-Type', 'application/json')
-            res.end(data)
-            break
-        default:
-            res.writeHead(404, 'Not Found')
-            res.end()
-    }
-    console.log('server started')
-    // res.setHeader('Dummy', 'Dummy header')
-
-})
-
-server.listen(8080)
+server.listen(8000)
