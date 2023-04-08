d3280f634add5f12a9c3ded665d5dfe8b97fb03c
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-04-08 07:51:26 +0530

    get post etc method

--- index.js
+++ index.js
@@ -7,4 +7,55 @@ const products = data.products;
 
 const server = express()
 
-server.listen(8000)
+server.use(express.json())
+
+server.use((req, res, next) => {
+    // console.log(req.method, req.ip, req.hostname, new Date(), req.get('User-Agent'))
+    next()
+})
+
+const auth = (req, res, next) => {
+    console.log(req.query)
+    if (req.body.password === '123') {
+
+        next()
+    } else {
+        res.sendStatus(401)
+    }
+
+}
+
+// server.use(auth)
+server.get('/', auth, (req, res) => {
+    res.json({ type: 'GET' })
+})
+
+server.post('/', (req, res) => {
+    res.json({ type: 'POST' })
+})
+
+server.put('/', (req, res) => {
+    res.json({ type: 'PUT' })
+})
+
+server.delete('/', (req, res) => {
+    res.json({ type: 'DELETE' })
+})
+
+
+server.patch('/', (req, res) => {
+    res.json({ type: 'PATCH' })
+})
+
+
+
+server.get('/demo', (req, res) => {
+    res.sendStatus(404)
+    // res.sendFile('index.html')
+    // res.json(data)
+})
+
+
+server.listen(8000, () => {
+    console.log('server started')
+})
