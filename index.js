d2ddfa3aa1c2f43b3ffdd66b3a6b271aeb54e081
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-04-11 07:15:51 +0530

    product

--- index.js
+++ index.js
@@ -1,61 +1,22 @@
-const fs = require('fs');
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
+const productController = require('./product')
+const server = express();
 
-const index = fs.readFileSync('index.html', 'utf-8');
-const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-const products = data.products;
 
-const server = express()
-
+//body parser
 server.use(express.json())
+server.use(morgan('default'))
+server.use(express.static('public'))
 
-server.use((req, res, next) => {
-    // console.log(req.method, req.ip, req.hostname, new Date(), req.get('User-Agent'))
-    next()
-})
+//MVC model-view-controller
+server.post('/products', productController.createProduct);
+server.get('/products', productController.getAllProducts);
+server.get('/products/:id', productController.getProduct);
+server.put('/products/:id', productController.replaceProduct);
+server.patch('/products/:id', productController.replaceProduct);
+server.delete('/products/:id', productController.deleteProduct);
 
-const auth = (req, res, next) => {
-    console.log(req.query)
-    if (req.body.password === '123') {
-
-        next()
-    } else {
-        res.sendStatus(401)
-    }
-
-}
-
-// server.use(auth)
-server.get('/', auth, (req, res) => {
-    res.json({ type: 'GET' })
-})
-
-server.post('/', (req, res) => {
-    res.json({ type: 'POST' })
-})
-
-server.put('/', (req, res) => {
-    res.json({ type: 'PUT' })
-})
-
-server.delete('/', (req, res) => {
-    res.json({ type: 'DELETE' })
-})
-
-
-server.patch('/', (req, res) => {
-    res.json({ type: 'PATCH' })
-})
-
-
-
-server.get('/demo', (req, res) => {
-    res.sendStatus(404)
-    // res.sendFile('index.html')
-    // res.json(data)
-})
-
-
-server.listen(8000, () => {
-    console.log('server started')
-})
+server.listen(8080, () => {
+    console.log('server started');
+});
