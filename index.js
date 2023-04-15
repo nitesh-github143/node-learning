41215f09610018fcd2ddd16b34ff0fabefa315be
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-04-15 06:02:36 +0530

    express

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-const express = require('express');
-const morgan = require('morgan');
-const productController = require('./product')
-const server = express();
+const fs = require('fs')
+const index = fs.readFileSync('index.html', 'utf-8')
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'))
+const products = data.products
 
+const express = require('express')
 
-//body parser
-server.use(express.json())
-server.use(morgan('default'))
-server.use(express.static('public'))
-
-//MVC model-view-controller
-server.post('/products', productController.createProduct);
-server.get('/products', productController.getAllProducts);
-server.get('/products/:id', productController.getProduct);
-server.put('/products/:id', productController.replaceProduct);
-server.patch('/products/:id', productController.replaceProduct);
-server.delete('/products/:id', productController.deleteProduct);
-
-server.listen(8080, () => {
-    console.log('server started');
-});
