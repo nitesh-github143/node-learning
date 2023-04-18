dbf54588284d46b1f84e6fdc8464bad9b3e7c453
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-04-18 14:24:24 +0530

    dot eve

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+require('dotenv').config()
+
+console.log(process.env.DB_PASSWORD)
 
 const server = express()
 const productRouter = require('./routes/product')
 server.use(express.json())
-server.use(express.static('public'))
+server.use(express.static(process.env.PUBLIC_DIR))
 server.use('/products', productRouter.router)
 
-server.listen(3000)
+server.listen(process.env.PORT, () => {
+    console.log('server started')
+})
