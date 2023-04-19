08b7b8f2ff60ea25194267fcb1d2739782c45218
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-04-19 06:10:50 +0530

    schema

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const productRouter = require('./routes/product')
 require('dotenv').config()
 
 console.log(process.env.DB_PASSWORD)
 
+//db connection 
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
+    console.log('hello nitesh')
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+//schema
+
+const productSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    discountPercentage: Number,
+    rating: Number,
+    brand: String,
+    category: String,
+    thumbnail: String,
+    images: [String]
+});
+
+const Product = mongoose.model('Product', productSchema)
+
 const server = express()
-const productRouter = require('./routes/product')
 server.use(express.json())
 server.use(express.static(process.env.PUBLIC_DIR))
 server.use('/products', productRouter.router)
